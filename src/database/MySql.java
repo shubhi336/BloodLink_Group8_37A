9188794e3a5edadaed0f7f0b35ce97ed6e84d6ff
shubhi336 <perisapadma@gmail.com>
2025-05-19 11:49:57 +0545

    dataase

---
+++ src/database/MySql.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+
+
+/**
+ *
+ * @author VICTUS
+ */
+public class MySql implements Database{
+
+    @Override
+    public Connection openConnection() {
+        try{
+            String username="root";
+            String password="Sos@20620426";
+            String database="testdatabase";           
+            Connection connection;
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/"+ database, username, password);
+                
+            if(connection==null){
+                System.out.println("Database connection fail");
+            }else{
+                System.out.println("Database connection success");
+            }
+            return connection;
+    }
+        catch(SQLException e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    @Override
+    public void CloseConnection(Connection conn) {
+        try{
+            if (conn != null && !conn.isClosed()){
+            conn.close();
+            System.out.println("Connection close");
+            }
+    }
+        catch(SQLException e){
+            System.out.println(e);
+            
+        }}
+
+    @Override
+    public ResultSet runQuery(Connection conn, String query) {
+        try{
+            Statement stmp = conn.createStatement();
+            ResultSet result = stmp.executeQuery(query);
+            return result;
+        } catch (SQLException e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    @Override
+    public int executeUpdate(Connection conn, String query) {
+        try{
+            Statement stmp= conn.createStatement();
+            int result = stmp.executeUpdate(query);
+            return result;
+        } catch(SQLException e){
+            System.out.println(e);
+            return -1;
+        }
+    }
+    
+}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+/**
+ *
+ * @author VICTUS
+ */
+public class MySql {
+    
+}
